8e10050b8534f2446946d1202cfbbe9d56c1bbb3
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2022-10-17 18:51:53 +0200

    fsdfs

---
+++ RestoreCord/Services/Database.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestoreCord.Schema;
+
+namespace RestoreCord.Services
+{
+    public class Database : DbContext
+    {
+        private readonly string ConnectionString = $"server=130.61.138.70;user=restorecord;database=restorecord;password={Properties.Resources.MySQLPass}";
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString)).UseLazyLoadingProxies();
+
+        public DbSet<Schema.Log.Errors> Errors { get; set; }
+        public DbSet<Member> members { get; set; }
+        public DbSet<Server> servers { get; set; }
+    }
+}
